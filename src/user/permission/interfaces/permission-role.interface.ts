54e64e4c25e504cd4d4bca716216bf500c6b9c99
Balázs Szabó <balazs.szabo@gmail.com>
2019-10-02 13:54:51 +0300

    delete permissions model

--- src/user/permission/interfaces/permission-role.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface IPermissionRole {
-  readonly id?: string;
-  readonly name?: string;
-  readonly description?: string;
-  readonly creationDate?: Date;
-  readonly updateDate?: Date;
-}
--- src/user/permission/interfaces/permission.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface IPermission {
-  readonly id?: string;
-  readonly name?: string;
-  readonly description?: string;
-  readonly creationDate?: Date;
-  readonly updateDate?: Date;
-}
