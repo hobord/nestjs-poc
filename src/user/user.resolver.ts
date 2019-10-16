0c46cb1be92adbc6d2e0773e20b9f4eea670372c
Balázs Szabó <balazs.szabo@gmail.com>
2019-10-16 13:22:40 +0300

    access roles documentation

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -24,63 +24,63 @@ export class UserResolver {
     return 'hello';
   }
 
-  @Query(returns => User, {description: 'Get the current authenticated user'})
+  @Query(returns => User, {description: 'Get the current authenticated user. Access roles:[authenticated]'})
   @Roles('authenticated')
   @UseGuards(GqlAuthGuard, GqlRolesGuard)
   whoAmI(@CurrentUser() user: User) {
     return this.userService.findOne(user.id);
   }
 
-  @Query(() => [User], {description: 'List of users'})
+  @Query(() => [User], {description: 'List of users. Access roles:[authenticated]'})
   @Roles('authenticated')
   @UseGuards(GqlAuthGuard, GqlRolesGuard) // { nullable: true }, 'paginate'
   async users(@Args({name: 'paginate', type: () => PaginateInput, nullable: true}) paginate?: PaginateInput) {
     return this.userService.findAll(paginate);
   }
 
-  @Query(() => User, {description: 'Get user by id'})
+  @Query(() => User, {description: 'Get user by id. Access roles:[authenticated]'})
   @Roles('authenticated')
   @UseGuards(GqlAuthGuard, GqlRolesGuard)
   async user(@Args('id') id: string) {
     return this.userService.findOne(id);
   }
 
-  @ResolveProperty(() => Roles, {description: 'Get user\'s roles'})
+  @ResolveProperty(() => Roles, {description: 'Get user\'s roles. Access roles:[authenticated]'})
   @Roles('authenticated')
   @UseGuards(GqlAuthGuard, GqlRolesGuard)
   async roles(@CurrentUser() user: User): Promise<string[]> {
     return await this.userService.getUserRoleNames(user);
   }
 
-  @Mutation(() => User, {description: 'Create a user'})
+  @Mutation(() => User, {description: 'Create a user. Access roles:[root, usermanager]'})
   @Roles('root', 'usermanager')
   @UseGuards(GqlAuthGuard, GqlRolesGuard)
   async createUser(@Args('input') input: UserInput) {
     return this.userService.create(input);
   }
 
-  @Mutation(() => User, {description: 'Update a user'})
+  @Mutation(() => User, {description: 'Update a user. Access roles:[root, usermanager]'})
   @Roles('root', 'usermanager')
   @UseGuards(GqlAuthGuard, GqlRolesGuard)
   async updateUser(@Args('id') id: string, @Args('input') input: UserInput) {
     return this.userService.update(id, input);
   }
 
-  @Mutation(() => User, {description: 'Update the current authenticated user'})
+  @Mutation(() => User, {description: 'Update the current authenticated user. Access roles:[authenticated]'})
   @Roles('authenticated')
   @UseGuards(GqlAuthGuard, GqlRolesGuard)
   async updateMe(@CurrentUser() user: User, @Args('input') input: UserInput) {
     return this.userService.update(user.id, input);
   }
 
-  @Mutation(() => User, {description: 'Delete a user'})
+  @Mutation(() => User, {description: 'Delete a user. Access roles:[root, usermanager]'})
   @Roles('root', 'usermanager')
   @UseGuards(GqlAuthGuard, GqlRolesGuard)
   async deleteUser(@Args('id') id: string) {
     return this.userService.delete(id);
   }
 
-  @Mutation(() => User, {description: 'Assign a permission role into the user'})
+  @Mutation(() => User, {description: 'Assign a permission role into the user. Access roles:[root, usermanager]'})
   @Roles('root', 'usermanager')
   @UseGuards(GqlAuthGuard, GqlRolesGuard)
   async addRoleToUser(
@@ -91,7 +91,7 @@ export class UserResolver {
     return this.userService.addUserRole(user, roleName);
   }
 
-  @Mutation(() => User, {description: 'Revoke a permission role into the user'})
+  @Mutation(() => User, {description: 'Revoke a permission role into the user. Access roles:[root, usermanager]'})
   @Roles('root', 'usermanager')
   @UseGuards(GqlAuthGuard, GqlRolesGuard)
   async removeRoleFromUser(
