60f88b39665b62e80770f8a2942e5ac5331c77c3
Balázs Szabó <balazs.szabo@gmail.com>
2019-10-16 15:04:38 +0300

    fix example

--- src/example/example.resolver.ts
+++ src/example/example.resolver.ts
@@ -2,6 +2,8 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { ExampleService } from './example.service';
 import { Example } from './dto/example.dto';
 import { ExampleInput } from './dto/input-example.input';
+import { PaginateInput } from '../common/pagination/paginate.input';
+import { OrderByInput } from '../common/order/order-by.input';
 
 @Resolver(of => Example)
 export class ExampleResolver {
@@ -10,8 +12,11 @@ export class ExampleResolver {
   ) {}
 
   @Query(() => [Example], {nullable: true})
-  async examples(): Promise<Example[]> {
-    return this.exampleService.getAll();
+  async examples(
+    @Args({name: 'paginate', type: () => PaginateInput, nullable: true}) paginate?: PaginateInput,
+    @Args({name: 'orderby', type: () => [OrderByInput], nullable: true}) orderBy?: OrderByInput[],
+  ): Promise<Example[]> {
+    return this.exampleService.getAll(paginate, orderBy);
   }
 
   @Query(() => Example, {nullable: true})
