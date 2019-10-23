d0f9ceea4f3ff07ea471e6c3a69733e93e8ea9d4
Balázs Szabó <balazs.szabo@gmail.com>
2019-10-23 14:29:53 +0300

    refreshToken to rest api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { RestAuthGuard } from './rest-auth.guard';
+import { RestRolesGuard } from './rest-roles.guard';
+import { Roles } from './roles.decoraqtor';
 
 @Controller('auth')
 export class AuthController {
@@ -9,6 +12,13 @@ export class AuthController {
   @UseGuards(AuthGuard('local'))
   @Post('login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @Get('refresh_token')
+  @UseGuards(RestAuthGuard, RestRolesGuard)
+  @Roles('authenticated')
+  async refreshToken(@Request() req) {
     return this.authService.login(req.user);
   }
 
