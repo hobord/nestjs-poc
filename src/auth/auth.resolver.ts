5c9ef20f46c7b74bde62977573aa2d759f3616b1
Balázs Szabó <balazs.szabo@gmail.com>
2019-10-23 14:27:05 +0300

    refreshToken

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -3,6 +3,12 @@ import { AuthService } from './auth.service';
 import { AuthResult } from './dto/auth-result.dto';
 import { Role } from './dto/role.dto';
 import { RoleService } from './role.service';
+import { Roles } from './roles.decoraqtor';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from './graphql-auth.guard';
+import { GqlRolesGuard } from './graphql-roles.guard';
+import { CurrentUser } from '../user/user.decorator';
+import { User } from '../user/dto/user.dto';
 
 @Resolver(of => AuthResult)
 export class AuthResolver {
@@ -17,6 +23,13 @@ export class AuthResolver {
     @Args('password') password: string,
   ) {
     const user = await this.authService.validateUser(username,  password);
+    return this.authService.login(user);
+  }
+
+  @Query(() => AuthResult, {description: 'Renew the jwt token'})
+  @UseGuards(GqlAuthGuard, GqlRolesGuard)
+  @Roles('authenticated')
+  async refreshToken(@CurrentUser() user: User) {
     return this.authService.login(user);
   }
 
